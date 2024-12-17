cd4821b21959de4eed9e90b23cd214296a416007
Spiky-73 <cocojeanne73@gmail.com>
2024-12-17 20:57:05 +0100

    fixed %o in formatting

--- 06-Guard_Gallivant/main.go
+++ 06-Guard_Gallivant/main.go
@@ -33,7 +33,7 @@ func parseGrid() ([]string, utils.Point) {
 		grid = append(grid, line)
 	}
 	utils.LogGrid(grid)
-	utils.LogF("Guard position: %o\r\n", pos)
+	utils.LogF("Guard position: %v\r\n", pos)
 
 	return grid, pos
 }
@@ -57,7 +57,64 @@ func part1() {
 		if !utils.IsInsideGrid(nPos, grid) {
 			break
 		}
+		if grid[nPos.Y][nPos.X] == '#' {
+			direction = (direction + 1) % len(deltas)
+			utils.LogF("Turn on %v\r\n", pos)
+			continue
+		}
+		pos = nPos
+		bytes := []byte(grid[pos.Y])
+		bytes[pos.X] = dirChar[direction]
+		grid[pos.Y] = string(bytes)
+		if !visited[pos.Y][pos.X] {
+			visited[pos.Y][pos.X] = true
+			numVisited += 1
+		}
+	}
+	utils.LogGrid(grid)
+
+	fmt.Printf("Visited cells: %d\r\n", numVisited)
+}
+
+func part2() {
+	grid, pos := parseGrid()
+
+	utils.Log("Simulating guard movement...")
+	var visited [][]bool
+	for i := 0; i < len(grid); i++ {
+		visited = append(visited, make([]bool, len(grid[i])))
+	}
+	visited[pos.Y][pos.X] = true
+	numVisited := 1
+	loops := 0
+
+	direction := 0
+	dirChar := "^>v<"
+	deltas := []utils.Point{{X: 0, Y: -1}, {X: 1, Y: 0}, {X: 0, Y: 1}, {X: -1, Y: 0}}
+	for {
+		nPos := utils.Point{X: pos.X + deltas[direction].X, Y: pos.Y + deltas[direction].Y}
+		if !utils.IsInsideGrid(nPos, grid) {
+			break
+		}
 		if grid[nPos.Y][nPos.X] != '#' {
+			lDirection := (direction + 1) % len(deltas)
+			lPos := pos
+			for {
+				plPos := lPos
+				lPos = utils.Point{X: lPos.X + deltas[lDirection].X, Y: lPos.Y + deltas[lDirection].Y}
+				if !utils.IsInsideGrid(lPos, grid) {
+					break
+				}
+				if grid[lPos.Y][lPos.X] != '#' {
+					continue
+				}
+				if visited[plPos.Y][plPos.X] {
+					utils.LogF("Possible loop on %v\r\n", pos)
+					utils.LogGrid(grid)
+					loops += 1
+				}
+				break
+			}
 			pos = nPos
 			bytes := []byte(grid[pos.Y])
 			bytes[pos.X] = dirChar[direction]
@@ -69,12 +126,10 @@ func part1() {
 			continue
 		}
 		direction = (direction + 1) % len(deltas)
-		utils.LogF("Turn on %o\r\n", pos)
+		utils.LogF("Turn on %v\r\n", pos)
 	}
 	utils.LogGrid(grid)
 
 	fmt.Printf("Visited cells: %d\r\n", numVisited)
-}
-
-func part2() {
+	fmt.Printf("Possible loops: %d\r\n", loops)
 }
